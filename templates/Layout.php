a300e77a43590ac8d4de5681d79b78ebdd161283
youcode safi <kaoutar@gmail.com>
2025-12-05 14:33:43 +0100

    creation de page layout et inclue le footer et header

---
+++ templates/Layout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    
+</head>
+<body>
+    <?php include __DIR__ . '/header.php'; ?>
+    <main class="h-[900px]">
+       <?php include $content; ?>
+
+    </main>
+
+     <?php include __DIR__ . '/footer.php'; ?>
+
+    
+    </body>
+</html>
